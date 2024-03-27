932d4a43f11f8113e9b3438e5464d952f350c38f
David Ballesteros <david_ballesteros@outlook.com>
2024-03-28 02:00:24 +0800

    Add screenshot function

--- TextExtractor.WPF/ViewModels/MainViewModel.cs
+++ TextExtractor.WPF/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -7,15 +10,16 @@ namespace TextExtractor.WPF.ViewModels;
 public partial class MainViewModel : ObservableObject
 {
     private IEnumerable<Screen> Screens { get; set; } = [];
-    
+
     [ObservableProperty] private ObservableCollection<string> _displayList = [];
     [ObservableProperty] private string? _selectedDisplay;
+    [ObservableProperty] private BitmapSource _displayImage;
 
     partial void OnSelectedDisplayChanged(string? value)
     {
-        ShowDisplayPreview();
+        DisplayScreenshot();
     }
-    
+
     public MainViewModel()
     {
         RefreshDisplayList();
@@ -39,10 +43,24 @@ public partial class MainViewModel : ObservableObject
     }
 
     [RelayCommand]
-    private void ShowDisplayPreview()
+    private void DisplayScreenshot()
     {
-        var selectedMonitor = Screens.FirstOrDefault(x => x.DeviceName.Equals(SelectedDisplay));
-        
+        var selectedMonitor = Screens.FirstOrDefault(x => x.DeviceName.Equals(SelectedDisplay))!;
+
         Console.WriteLine(selectedMonitor);
+
+        var left = selectedMonitor.Bounds.X;
+        var top = selectedMonitor.Bounds.Y;
+        var width = selectedMonitor.Bounds.Width;
+        var height = selectedMonitor.Bounds.Height;
+
+        using var screenBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        using var bmpGraphics = Graphics.FromImage(screenBmp);
+        bmpGraphics.CopyFromScreen(left, top, 0, 0, new System.Drawing.Size(width, height));
+        DisplayImage = Imaging.CreateBitmapSourceFromHBitmap(
+            screenBmp.GetHbitmap(),
+            IntPtr.Zero,
+            Int32Rect.Empty,
+            BitmapSizeOptions.FromEmptyOptions());
     }
 }
