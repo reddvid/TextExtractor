f7d982a60a353b11ffa49b95065ad062b667709e
David Ballesteros <david_ballesteros@outlook.com>
2024-03-28 01:49:26 +0800

    Add SelectedItem for Screens

--- TextExtractor.WPF/ViewModels/MainViewModel.cs
+++ TextExtractor.WPF/ViewModels/MainViewModel.cs
@@ -1,12 +1,21 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace TextExtractor.WPF.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
+    private IEnumerable<Screen> Screens { get; set; } = [];
+    
     [ObservableProperty] private ObservableCollection<string> _displayList = [];
+    [ObservableProperty] private string? _selectedDisplay;
 
+    partial void OnSelectedDisplayChanged(string? value)
+    {
+        ShowDisplayPreview();
+    }
+    
     public MainViewModel()
     {
         RefreshDisplayList();
@@ -14,18 +23,26 @@ public partial class MainViewModel : ObservableObject
 
     private void RefreshDisplayList()
     {
-        var monitors = Screen.AllScreens;
+        Screens = Screen.AllScreens;
 
-        if (monitors.Length != 0) DisplayItems(monitors);
+        if (Screens.Any()) DisplayItems();
     }
 
-    private void DisplayItems(IEnumerable<Screen> monitors)
+    private void DisplayItems()
     {
         DisplayList.Clear();
         ;
-        foreach (var monitor in monitors)
+        foreach (var monitor in Screens)
         {
             DisplayList.Add(monitor.DeviceName);
         }
+    }
+
+    [RelayCommand]
+    private void ShowDisplayPreview()
+    {
+        var selectedMonitor = Screens.FirstOrDefault(x => x.DeviceName.Equals(SelectedDisplay));
+        
+        Console.WriteLine(selectedMonitor);
     }
 }
